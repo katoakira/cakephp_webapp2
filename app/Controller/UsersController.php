46a97a1a486b28a01d1dd5764eb8655225680ac1
Akira Kato <akira.kato.19930831@icloud.com>
2015-01-09 15:56:28 +0000

    Add Login/Logout Action

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -3,8 +3,23 @@
 class UsersController extends AppController {
 
     public function beforeFilter() {
-        parent::beforeFilter();
-        $this->Auth->allow('add');
+       parent::beforeFilter();
+       // ユーザー自身による登録とログアウトを許可する
+       $this->Auth->allow('add', 'logout');
+    }
+   
+    public function login() {
+        if ($this->request->is('post')) {
+            if ($this->Auth->login()) {
+                $this->redirect($this->Auth->redirect());
+            } else {
+                $this->Session->setFlash(__('Invalid username or password, try again'));
+            }
+        }
+    }
+    
+    public function logout() {
+        $this->redirect($this->Auth->logout());
     }
 
     public function index() {
