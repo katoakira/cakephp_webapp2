7529974df0d122e0faea9407da83a825cfe949fc
Akira Kato <akira.kato.19930831@icloud.com>
2015-01-09 15:25:09 +0000

    Add Add Action

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -16,5 +16,16 @@ class PostsController extends AppController {
                throw new NotFoundException(__('Invalid post'));
            }
            $this->set('post', $post);
-       }
+    }
+
+    public function add() {
+        if ($this->request->is('post')) {
+            $this->Post->create();
+            if ($this->Post->save($this->request->data)) {
+                $this->Session->setFlash(__('Your post has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            }
+            $this->Session->setFlash(__('Unable to add your post.'));
+        }
+    }
 } 
