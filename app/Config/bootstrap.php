e04b1304f552788d542e09e889d084e542f9fd60
Akira Kato <akira.kato.19930831@icloud.com>
2015-01-09 14:40:12 +0000

    Conplete Setup

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -69,7 +69,7 @@ Cache::config('default', array('engine' => 'File'));
  * CakePlugin::load('DebugKit'); //Loads a single plugin named DebugKit
  *
  */
-
+CakePlugin::loadAll();
 /**
  * To prefer app translation over plugin translation, you can set
  *
--- app/Config/core.php
+++ app/Config/core.php
@@ -31,8 +31,8 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	Configure::write('debug', 2);
-
+//	Configure::write('debug', 2);
+    Configure::write('debug', 1);
 /**
  * Configure the Error handler used to handle errors for your application. By default
  * ErrorHandler::handleError() is used. It will display errors using Debugger, when debug > 0
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'cf0fd7478432eb00e7ee6dd0da034b0c664f0a26');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '751126113646742160473561405556');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
